702182718d6aec01c296de99e2e933eacab66a0f
MoniqChege <quichege33@gmail.com>
2025-04-28 17:04:15 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, inject, signal, ViewChild, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'goal-tracker';
   @ViewChild("loginModal") loginModal!: ElementRef;
+  isLoginFormVisible = signal<boolean>(true);
+
+  registerObj:any = {
+    "userId": 0,
+    "emailId": '',
+    "password": '',
+    "confirmPassword": '', 
+    "fullName": '',
+    "mobileNo": ''
+  }
+
+  http = inject(HttpClient);
+
+  toggleForm() {
+    this.isLoginFormVisible.set(!this.isLoginFormVisible())
+  }
 
   openModal() {
     if(this.loginModal) {
@@ -21,5 +39,14 @@ export class AppComponent {
     if(this.loginModal) {
       this.loginModal.nativeElement.style.display = 'none';
     }
+  }
+
+  onRegister(){
+    debugger
+    this.http.post("https://freeapi.miniprojectideas.com/api/Goals/createNewUser", this.registerObj).subscribe((res:any) => {
+      debugger
+      alert("Registration Successful")
+    }, error=>{
+     })
   }
 }
