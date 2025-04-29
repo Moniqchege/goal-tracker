6a0e44b866d8020aaee41f050e59fdd31e22984a
MoniqChege <quichege33@gmail.com>
2025-04-29 08:48:03 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,53 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, inject, signal, ViewChild, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { loginUser, RegisterUser } from './models/user.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'goal-tracker';
   @ViewChild("loginModal") loginModal!: ElementRef;
+  @ViewChild('navbarNav') navbarNav!: ElementRef;
   isLoginFormVisible = signal<boolean>(true);
 
-  registerObj:any = {
-    "userId": 0,
-    "emailId": '',
-    "password": '',
-    "confirmPassword": '', 
-    "fullName": '',
-    "mobileNo": ''
+  registerObj:RegisterUser = {
+    userId: 0,
+    email: '',
+    password: '',
+    confirmPassword: '', 
+    fullName: '',
+    mobileNo: ''
+  }
+
+  loginObj: loginUser = {
+    email: '',
+    password: ''
   }
 
   http = inject(HttpClient);
 
+  constructor(
+    private router: Router
+  ){
+    localStorage.removeItem('users');
+    localStorage.removeItem('loggedInUser');
+  }
+
   toggleForm() {
     this.isLoginFormVisible.set(!this.isLoginFormVisible())
+  }
+
+  closeNavbar() {
+    const navbar = this.navbarNav?.nativeElement;
+    if(navbar?.classList.contains('show')) {
+      navbar.classList.remove('show');
+    }
   }
 
   openModal() {
@@ -42,9 +63,34 @@ export class AppComponent {
   }
 
   onRegister(){
-    this.http.post("https://freeapi.miniprojectideas.com/api/Goals/createNewUser", this.registerObj).subscribe((res:any) => {
-      alert("Registration Successful")
-    }, error=>{
-     })
+    const existingUsers = JSON.parse(localStorage.getItem('users') || '[]');
+
+    const userExists = existingUsers.some((user: any) => user.email === this.registerObj.email);
+    if (userExists) {
+      alert('User email already exists');
+      return;
+    }
+
+    existingUsers.push(this.registerObj);
+
+    localStorage.setItem('users', JSON.stringify(existingUsers));
+    alert('Registration Successful');
+    this.isLoginFormVisible.set(true);
+
+    // this.registerObj = {RegisterUser};
+  }
+
+  onLogin(){
+    const existingUsers = JSON.parse(localStorage.getItem('users') || '[]');
+
+    const user = existingUsers.find((u:any)=>u.email === this.loginObj.email && u.password === this.loginObj.password);
+    if(user) {
+      localStorage.setItem('loggedInUser', JSON.stringify(user));
+
+      this.closeModal();
+      this.router.navigate(['/dashboard'])
+    }else{
+      alert('Invalid email or password')
+    }
   }
 }
